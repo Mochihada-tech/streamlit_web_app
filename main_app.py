025da8f8b6037a58ac6b53e8ee8f2a2f4464ab5d
Mochihada-tech <gxxxxxmail@gmail.com>
2024-05-05 21:54:04 +0900

    add png

--- main_app.py
+++ main_app.py
@@ -7,3 +7,6 @@ st.caption('This is test apps')
 
 image = Image.open('./data/dog.jpg')
 st.image(image, width=200)
+
+image2 = Image.open('./data/git-new-repository.png')
+st.image(image2, width=300)
